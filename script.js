d8d1143d50bb4c6bb8ddeb002672573e9b137573
buttonpusher <anthonymrondina@gmail.com>
2022-02-07 22:11:41 -0500

    further fleshed out player class and beginning to add math for damage

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+myPokemonCry = document.querySelector(".pCry")
+enemyPokemonCry = document.querySelector(".eCry")
+
 class Player {
     constructor(name) {
         this.name = name;
@@ -17,11 +20,13 @@ class Player {
         this.antidote = 5
         this.team = [{
             name: "Charizard",
+            cry: "sound.wav",
             type: ["fire", "flying"],
             weakness: ["rock", "water"],
             resist: ["fire"],
             img = "img.src",
-            poisined: false,
+            fainted: false,
+            poisoned: false,
             paralyzed: false,
             burned: false,
             frozen: 0,
@@ -37,15 +42,14 @@ class Player {
             speed: 122,
             moves: [{
                 name: "FlameThrower",
-                physical: false,
                 type: "fire",
-                // strength: ["grass", "bug", "grass", "ice"],
-                // resist: ["rock", "fire", "water", "dragon"],
+                physical: false,
                 special: true,
                 status: true,
+                statusType: "burn",
                 pp: 20,
                 power: 90,
-                accuracy: 100
+                accuracy: 1
             },
             {
                 name: "Wing Attack",
@@ -55,7 +59,7 @@ class Player {
                 status: false,
                 pp: 35,
                 power: 60,
-                accuracy: 100,
+                accuracy: 1,
             },
             {
                 name: "Fire Blast",
@@ -63,9 +67,10 @@ class Player {
                 physical: false,
                 special: true,
                 status: true,
+                statusType: "burn",
                 pp: 5,
                 power: 120,
-                accuracy: 85,
+                accuracy: .85,
             },
             {
                 name: "Aerial Ace",
@@ -75,16 +80,18 @@ class Player {
                 status: false,
                 pp: 20,
                 power: 60,
-                accuracy: 100,
+                accuracy: 1,
             }]
         },
         {
             name: "Venusaur",
             type: "grass",
+            cry: "sound.wav",
             weakness: ["fire"],
             resist: ["grass", "water"],
             img = "img.src",
-            poisined: false,
+            fainted: false,
+            poisoned: false,
             paralyzed: false,
             burned: false,
             frozen: 0,
@@ -108,39 +115,45 @@ class Player {
                 accuracy: 95
             },
             {
-                name: "Poisin Powder",
+                name: "poison Powder",
+                type: "poison",
                 physical: false,
                 special: false,
                 status: true,
+                statusType: "poison",
                 pp: 35,
                 power: 0,
-                accuracy: 75,
+                accuracy: .75,
             },
             {
                 name: "Leech Seed",
+                type: "grass",
                 physical: false,
                 special: false,
                 status: true,
                 pp: 10,
                 power: 0,
-                accuracy: 90,
+                accuracy: .9,
             },
             {
                 name: "Vine Whip",
+                type: "grass",
                 physical: true,
                 special: false,
                 status: false,
                 loseTurn: false,
                 pp: 25,
                 power: 45,
-                accuracy: 100,
+                accuracy: 1,
             }]
         },
         {
             name: "Blastoise",
             type: "water",
+            cry: "sound.wav",
             img = "img.src",
-            poisined: false,
+            fainted: false,
+            poisoned: false,
             paralyzed: false,
             burned: false,
             frozen: 0,
@@ -156,46 +169,52 @@ class Player {
             speed: 100,
             moves: [{
                 name: "Hydro Pump",
+                type: "water",
                 physical: false,
                 special: true,
                 status: false,
                 pp: 5,
                 power: 120,
-                accuracy: 80
+                accuracy: .8
             },
             {
                 name: "Surf",
+                type: "water",
                 physical: false,
                 special: true,
                 status: false,
                 pp: 15,
                 power: 90,
-                accuracy: 100,
+                accuracy: 1,
             },
             {
                 name: "Earthquake",
+                type: "ground",
                 physical: true,
                 special: false,
                 status: false,
                 pp: 10,
                 power: 100,
-                accuracy: 100,
+                accuracy: 1,
             },
             {
                 name: "Water Gun",
+                type: "water",
                 physical: false,
                 special: true,
                 status: false,
                 loseTurn: false,
                 pp: 25,
                 power: 40,
-                accuracy: 100,
+                accuracy: 1,
             }]
         }, {
             name: "Alakazam",
             type: "psychic",
+            cry: "sound.wav",
             img = "img.src",
-            poisined: false,
+            fainted: false,
+            poisoned: false,
             paralyzed: false,
             burned: false,
             frozen: 0,
@@ -211,46 +230,53 @@ class Player {
             speed: 143,
             moves: [{
                 name: "Psychic",
+                type: "psychic",
                 physical: false,
                 special: true,
                 status: false,
                 pp: 10,
                 power: 90,
-                accuracy: 100
+                accuracy: 1
             },
             {
                 name: "Psybeam",
+                type: "psychic",
                 physical: false,
                 special: true,
                 status: false,
                 pp: 20,
                 power: 65,
-                accuracy: 100,
+                accuracy: 1,
             },
             {
                 name: "Confusion",
+                type: "psychic",
                 physical: false,
                 special: true,
                 status: true,
+                statusType: "confusion",
                 pp: 20,
                 power: 50,
-                accuracy: 100,
+                accuracy: 1,
             },
             {
                 name: "Recover",
+                type: "psychic",
                 physical: false,
                 special: false,
                 status: true,
                 loseTurn: false,
                 pp: 10,
                 power: 0,
-                accuracy: 100,
+                accuracy: 1,
             }]
         }, {
             name: "Lapras",
             type: ["water", "ice"],
+            cry: "sound.wav",
             img = "img.src",
-            poisined: false,
+            fainted: false,
+            poisoned: false,
             paralyzed: false,
             burned: false,
             frozen: 0,
@@ -266,54 +292,163 @@ class Player {
             speed: 82,
             moves: [{
                 name: "Ice Beam",
+                type: "ice",
                 physical: false,
                 special: true,
                 status: true,
+                statusType: "frozen",
                 pp: 10,
                 power: 90,
-                accuracy: 100
+                accuracy: 1
             },
             {
                 name: "Sing",
+                type: "normal",
                 physical: false,
                 special: false,
                 status: true,
+                statusType: "sleep",
                 pp: 15,
                 power: 0,
-                accuracy: 0,
+                accuracy: 1,
             },
             {
                 name: "Surf",
+                type: "water",
                 physical: false,
                 special: true,
                 status: true,
                 pp: 5,
                 power: 95,
-                accuracy: 100,
+                accuracy: 1,
             },
             {
                 name: "Blizzard",
+                type: "ice",
                 physical: false,
                 special: true,
                 status: true,
+                statusType: "frozen",
                 loseTurn: false,
                 pp: 5,
                 power: 110,
-                accuracy: 75,
+                accuracy: .75,
             }]
         }]
     }
-    chooseItem() {
-        this.item = "antidote"
-    }
-    useItem() {
-        switch (this.item) {
+    chooseItem(evt) {
+        switch (evt.target.id) {
             case "antidote":
-                this.team[0].poisined = false
+                this.item = "antidote"
+                break;
+            case "fullRestore":
+                this.item = "fullRestore"
+                break;
+            case "revive":
+                this.item = "revive"
+                break;
+            case "parHeal":
+                this.item = "parHeal"
+                break;
+            case "burnHeal":
+                this.item = "burnHeal"
+                break;
+            case "unfreeze":
+                this.item = "unfreeze"
+                break;
+            case "awaken":
+                this.item = "awaken"
+                break;
+            case "confuseHeal":
+                this.item = "confuseHeal"
                 break;
         }
     }
-    checkStatus(yourPokemon) {
+    useItem(item, pokemon) {
+        switch (item) {
+            case "antidote":
+                if (this.antidote === 0) {
+                    console.log('none')
+                } else {
+                    pokemon.poisoned = false
+                    console.log("you are no longer poisoned!")
+                    this.antidote--
+                    //switch to enemy turn
+                }
+                break;
+            case "fullRestore":
+                if (this.fullRestore === 0) {
+                    console.log('none')
+                } else {
+                    pokemon.hp = this.team[0].totalHP
+                    console.log("you are healed!")
+                    this.fullRestore--
+                    //switch to enemy turn
+                }
+                break;
+            case "revive":
+                if (this.revives === 0) {
+                    console.log('none')
+                } else {
+                    pokemon.fainted = false
+                    pokemon.hp = (this.team[0].totalHP / 2)
+                    console.log("you are no longer poisoned!")
+                    this.revives--
+                    //switch to enemy turn
+                }
+                break;
+            case "parHeal":
+                if (this.parHeal === 0) {
+                    console.log('none')
+                } else {
+                    pokemon.paralyzed = false
+                    console.log("you are healed!")
+                    this.parHeal--
+                    //switch to enemy turn              
+                } break;
+            case "burnHeal":
+                if (this.burnHeal === 0) {
+                    console.log('none')
+                } else {
+                    pokemon.burned = false
+                    console.log("you are healed!")
+                    this.burnHeal--
+                    //switch to enemy turn
+                }
+                break;
+            case "unfreeze":
+                if (this.unfreeze === 0) {
+                    console.log('none')
+                } else {
+                    pokemon.frozen = false
+                    console.log("you are healed!")
+                    this.unfreeze--
+                    //switch to enemy turn
+                }
+                break;
+            case "awaken":
+                if (this.awaken === 0) {
+                    console.log('none')
+                } else {
+                    pokemon.asleep = false
+                    console.log("you are healed!")
+                    this.awaken--
+                    //switch to enemy turn
+                }
+                break;
+            case "confuseHeal":
+                if (this.confuseHeal === 0) {
+                    console.log('none')
+                } else {
+                    pokemon.confused = 0
+                    console.log("you are healed!")
+                    this.confuseHeal--
+                    //switch to enemy turn
+                }
+                break;
+        }
+    }
+    preCheckStatus(yourPokemon) {
         //Check if confused
         if (yourPokemon.confused) {
             //hit yourself
@@ -343,20 +478,25 @@ class Player {
             this.attack(this.attackChoice, enemy.team[0])
         }
     }
+    //method to switch to next elite 4 member when previous is defeated
     attack(chosenAttack, targetPokemon) {
-        //switch to make sure of target
+        let attack = 0
         switch (this.target) {
             case "Lorelei":
                 targetPokemon = Lorelei.team[0]
+                this.target = Lorelei.team[0]
                 break;
             case "Bruno":
                 targetPokemon = Bruno.team[0]
+                this.target = Bruno.team[0]
                 break;
             case "Agatha":
                 targetPokemon = Agatha.team[0]
+                this.target = Agatha.team[0]
                 break;
             case "Lance":
                 targetPokemon = Lance.team[0]
+                this.target = Lance.team[0]
                 break;
         }
         if (chosenAttack.power === 0) {
@@ -369,9 +509,18 @@ class Player {
                     break;
             }
         }
+
+        //set damage if physical
+        if (chosenAttack.physical) {
+            // damage = Math.floor((this.team[0].level / 5) + 2 * (this.team[0].moves[0].power * (this.team[0].attack / targetPokemon.defense) / 50)
+            //set damage if special
+        } else if (chosenAttack.special) {
+            // damage = Math.floor((this.team[0].level / 5) + 2 * (this.team[0].moves[0].power * (this.team[0].specialAttack / targetPokemon.specialDefense) / 50)
+        }
+
         //check if move hits
         index = Math.random()
-        if (index < chosenAttack.accuracy) {
+        if (index > chosenAttack.accuracy) {
             //check if move is Same Type Attack Bonus
             if (chosenAttack.type.includes(this.team[0].type)) {
                 console.log("this move is STAB and gets a 50% damage bonus")
@@ -380,40 +529,61 @@ class Player {
             //Check if target pokemon is weak or resistant to attack type
             if (targetPokemon.weakness.includes(chosenAttack.type)) {
                 console.log("It's super effective!")
-                chosenAttack.power * 2
+                damage *= 2
             } else if (targetPokemon.resist.includes(chosenAttack.type)) {
                 console.log("It's not very effective.")
-                chosenAttack.power / 2
+                damage /= 2
             }
 
-            //Find the code for how to apply damage of chosenAttack.power
-
-            //find the % of a crit chance
-
+            //Crit Chance is 6.25
+            let index = Math.random()
+            if (index > .93) {
+                damage *= 2
+            }
+            //apply damage  
+            targetPokemon.hp -= damage
+            console.log(damage)
 
         } else {
             console.log('you missed!')
+            //end player turn
         }
     }
-    checkYourStatusEnd(yourPokemon) {
+    applyStatus(chosenMove) {
+        switch (chosenMove.statusType) {
+            case "frozen":
+                this.target.frozen = true
+                break;
+            case "confusion":
+                this.target.confused = true
+                break;
+            case "sleep":
+                this.target.asleep = true
+                break;
+            case "poisoned":
+                this.target.poisoned = true
+                break;
+        }
+    }
+    postCheckYourStatus(yourPokemon) {
         //check your pokemon first
-        if (yourPokemon.poisined) {
+        if (yourPokemon.poisoned) {
             yourPokemon.hp -= (yourPokemon.totalHP * 0.0625) // 1/16th
             //CODE FOR DOM UPDATE
             setTimeout(() => {
-                this.checkTheirStatusEnd()
+                this.postCheckTheirStatus()
             }, 10000);
         } else if (yourPokemon.burned) {
             yourPokemon.hp -= (yourPokemon.totalHP * 0.0625) // 1/16th
             //CODE FOR DOM UPDATE
             setTimeout(() => {
-                this.checkTheirStatusEnd()
+                this.postCheckTheirStatus()
             }, 10000);
         }
     }
-    checkTheirStatusEnd(targetPokemon) {
+    postCheckTheirStatus(targetPokemon) {
         //check target pokemon second
-        if (targetPokemon.poisined) {
+        if (targetPokemon.poisoned) {
             targetPokemon.hp -= (targetPokemon.totalHP * 0.0625) // 1/16th
         } else if (targetPokemon.burned) {
             targetPokemon.hp -= (targetPokemon.totalHP * 0.0625) // 1/16th
@@ -434,4 +604,3 @@ class Player {
         }
     }
 }
-
